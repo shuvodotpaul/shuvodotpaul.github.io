5f8f945ed92f63f4c55e0aa673ac07ec0e1a8aeb
Shuvo Paul <iam_shuvo@yahoo.com>
2016-06-26 00:28:02 +0600

    avoid jquery conflict with other framework

--- cdn/candela-pricing-table/js/app.js
+++ cdn/candela-pricing-table/js/app.js
@@ -1,29 +1,31 @@
-$(document).ready(function() {
-	$('.table-tab li button').click(function(event) {
+
+$.noConflict();
+jQuery(document).ready(function() {
+	jQuery('.table-tab li button').click(function(event) {
 		/* Act on the event */
 		// tab
 		var deactivated = $('.active-tab').index();
 		console.log(deactivated);
 		var activated = $(this).parent().index();
 		console.log(activated);
-		$('.table-tab li').removeClass();
-		$(this).parent().addClass('active-tab');
-		$('#hide').hide();
+		jQuery('.table-tab li').removeClass();
+		jQuery(this).parent().addClass('active-tab');
+		jQuery('#hide').hide();
 
 
 		// table
-		$('.table').hide();
+		jQuery('.table').hide();
 		var idNumber = $(this).data('id');
 		if ( activated > deactivated) {
-			$(idNumber).show("slide", {direction: "up" }, 200 );
+			jQuery(idNumber).show("slide", {direction: "up" }, 200 );
 			console.log('bigger');
 		}
 		if ( activated < deactivated) {
-			$(idNumber).show("slide", {direction: "down" }, 200 );
+			jQuery(idNumber).show("slide", {direction: "down" }, 200 );
 			console.log('smaller');
 		}
 		else {
-			$(idNumber).show();
+			jQuery(idNumber).show();
 		}
 	});
 });																								
